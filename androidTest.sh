054e90549c33d4092212608b16c6861c4c211ca6
Majid <mhadaily@gmail.com>
2021-01-15 00:08:42 +0100

    android test app builds correctly

--- androidTest.sh
+++ androidTest.sh
@@ -1,5 +1,5 @@
 pushd android
-# flutter build generates files in android/ for building the app
+# # flutter build generates files in android/ for building the app
 flutter build apk
 ./gradlew app:assembleAndroidTest
 ./gradlew app:assembleDebug -Ptarget=integration_test/app_test.dart
@@ -11,8 +11,8 @@ gcloud --quiet config set project wiredbraincoffee-a1386
 
 gcloud firebase test android run --type instrumentation \
   --app build/app/outputs/apk/debug/app-debug.apk \
-  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk
-  # --timeout 2m \
+  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+  --timeout 2m \
   # --results-bucket=<RESULTS_BUCKET> \
   # --results-dir=<RESULTS_DIRECTORY>
 
